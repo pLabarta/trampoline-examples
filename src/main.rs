e44c2f6546364bf79ec583330334bb880ed1e768
plabarta <pablitolabarta@gmail.com>
2022-05-11 17:21:20 -0300

    fix: genesis_info removed

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ use ckb_sdk::traits::{
 use ckb_sdk::tx_builder::transfer::CapacityTransferBuilder;
 use ckb_sdk::tx_builder::{CapacityBalancer, TxBuilder};
 use ckb_sdk::unlock::{ScriptUnlocker, SecpSighashScriptSigner, SecpSighashUnlocker};
-use ckb_sdk::{GenesisInfo, ScriptId};
+use ckb_sdk::{ScriptId};
 use ckb_types::bytes::Bytes;
 use ckb_types::core::ScriptHashType;
 use ckb_types::packed::{Byte32, CellOutput, Script, ScriptBuilder, WitnessArgs};
@@ -120,9 +120,7 @@ impl TrampolineProvider {
     pub fn cell_dep_resolver(&self) -> DefaultCellDepResolver {
         let mut ckb_client = CkbRpcClient::new(self.node_url.as_str());
         let genesis_block = ckb_client.get_block_by_number(0.into()).unwrap().unwrap();
-        let info = GenesisInfo::from_block(&ckb_types::core::BlockView::from(genesis_block))
-            .expect("Failed creating genesis info from block");
-        DefaultCellDepResolver::new(&info)
+        DefaultCellDepResolver::from_genesis(&genesis_block.into()).expect("Failed creating genesis info from block")
     }
 
     pub fn header_dep_resolver(&self) -> DefaultHeaderDepResolver {
